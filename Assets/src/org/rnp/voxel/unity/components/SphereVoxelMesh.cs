377ae72d9c968d8d04791dee92ff49fef7981351
vyrval <alex77940@live.fr>
2016-02-08 21:15:23 +0100

    Test for colors and shaders

--- Assets/src/org/rnp/voxel/unity/components/SphereVoxelMesh.cs
+++ Assets/src/org/rnp/voxel/unity/components/SphereVoxelMesh.cs
@@ -1,19 +1,19 @@
-﻿using org.rnp.voxel.mesh;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
+﻿    using org.rnp.voxel.mesh;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
 
-namespace org.rnp.voxel.unity.components
-{
-  /// <author>Cédric DEMONGIVERT [cedric.demongivert@gmail.com]</author>
-  /// <summary>
-  ///   A simple, procedural, spheric voxel mesh
-  /// </summary>
-  [ExecuteInEditMode]
-  public class SphereVoxelMesh : VoxelMesh
-  {
+    namespace org.rnp.voxel.unity.components
+    {
+    /// <author>Cédric DEMONGIVERT [cedric.demongivert@gmail.com]</author>
+    /// <summary>
+    ///   A simple, procedural, spheric voxel mesh
+    /// </summary>
+    [ExecuteInEditMode]
+    public class SphereVoxelMesh : VoxelMesh
+    {
     /// <summary>
     ///   Sphere radius in voxels.
     /// </summary>
@@ -30,36 +30,36 @@ namespace org.rnp.voxel.unity.components
     ///   Sphere radius in voxels.
     /// </summary>
     public int Radius {
-      get
-      {
+        get
+        {
         return _Radius;
-      }
-      set
-      {
+        }
+        set
+        {
         this._Radius = value;
         this.RefreshMesh();
-      }
+        }
     }
 
     /// <summary>
     ///   Sphere color.
     /// </summary>
     public Color32 Color {
-      get
-      {
+        get
+        {
         return _Color;
-      }
-      set
-      {
+        }
+        set
+        {
         this._Color = value;
         this.RefreshMesh();
-      }
+        }
     }
 
     /// <see cref="http://docs.unity3d.com/ScriptReference/MonoBehaviour.html"/>
     public override void Awake()
     {
-      this.RefreshMesh();
+        this.RefreshMesh();
     }
 
     /// <summary>
@@ -71,11 +71,11 @@ namespace org.rnp.voxel.unity.components
     /// <returns></returns>
     protected Vector3 GetPoint(int x, int y, int z)
     {
-      return (new Vector3(
+        return (new Vector3(
         (float)(x - this.Radius) + 0.5f,
         (float)(y - this.Radius) + 0.5f,
         (float)(z - this.Radius) + 0.5f
-      ));
+        ));
     }
 
     /// <summary>
@@ -83,32 +83,36 @@ namespace org.rnp.voxel.unity.components
     /// </summary>
     public void RefreshMesh()
     {
-      int size = this.Radius * 2;
-      Color32 empty = new Color32(0, 0, 0, 255);
+        int size = this.Radius * 2;
+        Color32 empty = new Color32(0, 0, 0, 255);
 
-      VoxelArray sphere = new VoxelArray(size, size, size);
+        VoxelArray sphere = new VoxelArray(size, size, size);
 
-      for(int x = 0; x < size; ++x) 
-      {
-        for(int y = 0; y < size; ++y) 
+        Color32 currColor = new Color32((byte)(this._Color.r), (byte)(this._Color.g), (byte)(this._Color.b ), 0);
+
+        for (int x = 0; x < size; ++x) 
         {
-          for(int z = 0; z < size; ++z) 
-          {
-            Vector3 point = this.GetPoint(x, y, z);
+            for(int y = 0; y < size; ++y) 
+            {
+                for(int z = 0; z < size; ++z) 
+                {
+                    Vector3 point = this.GetPoint(x, y, z);
 
-            if (point.magnitude <= this.Radius)
-            {
-              sphere[x, y, z] = this.Color;
+                    if (point.magnitude <= this.Radius)
+                    {
+                        currColor = new Color32((byte)(UnityEngine.Random.Range(0,255)), (byte)(UnityEngine.Random.Range(0, 255)), (byte)(UnityEngine.Random.Range(0, 255)), 0);
+                        sphere[x, y, z] = currColor;
+                        System.Console.WriteLine(currColor.ToString());
+                    }
+                    else 
+                    {
+                        sphere[x, y, z] = empty;
+                    }
+                }
             }
-            else 
-            {
-              sphere[x, y, z] = empty;
-            }
-          }
         }
-      }
 
-      this.Mesh = sphere;
+        this.Mesh = sphere;
     }
-  }
+    }
 }
