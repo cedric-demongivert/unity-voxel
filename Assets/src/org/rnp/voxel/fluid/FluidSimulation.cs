a013fd74c9842876b640d06912303f8ede12c52b
Cédric DEMONGIVERT <cedric.demongivert@gmail.com>
2016-04-13 08:56:43 +0200

    Fluid Simulation base class

---
+++ Assets/src/org/rnp/voxel/fluid/FluidSimulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace org.rnp.voxel.fluid
+{
+  public abstract class FluidSimulation : VoxelMeshContainer
+  {
+    public abstract void Simulate(float duration);
+
+    public abstract void Add(float quantity, Vector3 location, Vector3 speed);
+
+    public abstract void Remove(float quantity, Vector3 location);
+  }
+}
