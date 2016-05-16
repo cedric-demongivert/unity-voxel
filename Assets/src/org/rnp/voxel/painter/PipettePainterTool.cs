33207876dff64648ff44d138689ac305cea58e5d
Cedric DEMONGIVERT <cedric.demongivert@gmail.com>
2016-05-16 18:09:28 +0200

    Pipette

---
+++ Assets/src/org/rnp/voxel/painter/PipettePainterTool.cs
@@ -0,0 +1,42 @@
+﻿using org.rnp.voxel.mesh;
+using org.rnp.voxel.physics;
+using org.rnp.voxel.translator;
+using org.rnp.voxel.utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace org.rnp.voxel.painter
+{
+  public class PipettePainterTool : PainterTool
+  {
+    public VoxelFilter filter;
+
+    public override void OnToolEnd()
+    {
+    }
+
+    public override void OnToolStart()
+    {
+    }
+
+    public override void OnToolUpdate()
+    {
+      if(Input.GetMouseButtonDown(0) && !(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)))
+      {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        VoxelHit result; 
+
+        if (VoxelPhysics.IsRayCollideVoxelMesh(ray, filter, out result))
+        {
+          Color32 color = this.Parent.PaintedMesh[result.HittedVoxel];
+          color.a = 255;
+
+          this.Parent.Picker.PickedColor = color;
+        }
+      }
+    }
+  }
+}
